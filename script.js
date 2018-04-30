369b40937bdb4d522249ef116323c336e662f288
Jakerjohnson94 <jacobrichardjohnson94@gmail.com>
2018-04-30 11:52:48 -0400

    added bar chart

--- script.js
+++ script.js
@@ -1,33 +1,37 @@
-const arr = Array.apply(null, Array(11)).map(()=> i, i=0);
+const arr = Array.apply(null, Array(11)).map(() => i, (i = 0));
 
-function rollDice(){
-    const min = Math.ceil(1);
-    const max = Math.floor(7);
-    return r = Math.floor(Math.random() * (max-min))+min;
-
-  
+function rollDice() {
+  const min = Math.ceil(1);
+  const max = Math.floor(7);
+  return (r = Math.floor(Math.random() * (max - min)) + min);
 }
 
-function getDiceValues(number){
-    for(let i=1; i<=number; i++){
-        
-        const roll = rollDice()+rollDice();
-        arr[roll-2]++;
+function getDiceValues(number) {
+  for (let i = 1; i <= number; i++) {
+    const roll = rollDice() + rollDice();
+    arr[roll - 2]++;
+  }
+  return arr;
+}
+function printValues() {
+  for (i = 0; i < arr.length; i++) {
+    const div = document.createElement("div");
+   const wrap =  document.getElementById("wrap")
+    div.textContent = `${i + 2}: ${arr[i]}`;
+    wrap.appendChild(div);
+  }
+}
+function drawGraph(){
+    for(let i=0; i<arr.length; i++){
+        const div = document.createElement("div");
+        const wrap2 = document.getElementById("wrap2");
+       div.setAttribute('style', `height: ${arr[i]}px`);
+        div.classList.add('bar');
+         wrap2.appendChild(div);
+     
     }
-    return arr;
 }
-    function printValues(){
-        for (i=0; i< arr.length; i++){
-       const div = document.createElement('div')
-       div.textContent = `${i+2}: ${arr[i]}`;
-       document.body.appendChild(div);
-           
-      
-
-    }
- 
-}
-
 
 getDiceValues(1000);
+
 console.log(arr);
